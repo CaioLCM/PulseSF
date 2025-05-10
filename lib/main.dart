73674d047032e1e85adcba621cb4b58c8e8eb1fe
Caio Lene Campos Magalhães <caio.lene.magalhaes@gmail.com>
2025-05-10 15:44:43 -0300

    Added email input and password input

--- lib/main.dart
+++ lib/main.dart
@@ -7,13 +7,40 @@ main(){
 class MyApp extends StatelessWidget {
     @override
   Widget build(BuildContext context) {
+    final emailController = new TextEditingController();
+    final passwordController = new TextEditingController();
     return(
       MaterialApp(home: 
         Scaffold(
-          appBar: AppBar(
-            title: Text("PulseSF"),
+            body: 
+            Center(
+              child: Column(
+                children: [
+                  SizedBox(height: 80),
+                  Text("PulseSF", style: TextStyle(
+                    color: Colors.purple[400],
+                    fontSize: 80,
+                    fontWeight: FontWeight.bold
+                    ),
+                    ),
+                    SizedBox(height: 30),
+                    TextField(
+                      controller: emailController,
+                      decoration: InputDecoration(
+                        labelText: "Email",
+                      ),
+                    ),
+                    SizedBox(height: 15),
+                    TextField(
+                      controller: passwordController,
+                      decoration: InputDecoration(
+                        labelText: "Password"
+                      ),
+                    )
+                  ],
+              ),
             ),
-          body: Text("This is the beggining of PulseSF!!!"),
+            
         )
       ,)
     );
