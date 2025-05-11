1b93b3024ae5c6d58917b1663662dcf5d4078fe9
Caio Lene Campos Magalhães <caio.lene.magalhaes@gmail.com>
2025-05-11 09:41:51 -0300

    Put some style at sign up page

--- lib/pages/Signupscreen.dart
+++ lib/pages/Signupscreen.dart
@@ -1,34 +1,74 @@
 import 'package:flutter/material.dart';
 
 class Signupscreen extends StatelessWidget{
+  
+  TextEditingController user = new TextEditingController();
+  TextEditingController email = new TextEditingController();
+  TextEditingController password = new TextEditingController();
+  TextEditingController confirmPassword = new TextEditingController();
   @override
   Widget build(BuildContext context){
     return Scaffold(
+      backgroundColor: Colors.white,
         body: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             Text("Create account"),
-            TextField(decoration: InputDecoration(
+            TextField(
+              controller: user,
+              decoration: InputDecoration(
               labelText: "Name",
-              icon: Icon(Icons.person)
+              prefixIcon: Icon(Icons.person),
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(13) 
+              ),
+              filled: true,
+              fillColor: Colors.grey[100]
             ),
             ),
-            TextField(decoration: InputDecoration(
+            SizedBox(height: 15,),
+            TextField(
+              controller: email,
+              decoration: InputDecoration(
               labelText: "Email",
-              icon: Icon(Icons.email)
+              prefixIcon: Icon(Icons.email),
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(13) 
+              ),
+              filled: true,
+              fillColor: Colors.grey[100]
             ),
             ),
-            TextField(decoration: InputDecoration(
+            SizedBox(height: 15,),
+            TextField(controller: password, decoration: InputDecoration(
               labelText: "Password",
-              icon: Icon(Icons.lock)
+              prefixIcon: Icon(Icons.password),
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(13) 
+              ),
+              filled: true,
+              fillColor: Colors.grey[100]
             ),
             ),
-            TextField(decoration: InputDecoration(
+            SizedBox(height: 15,),
+            TextField(
+              controller: confirmPassword,
+              decoration:InputDecoration(
               labelText: "Comfirm Password",
-              icon: Icon(Icons.lock)
+              prefixIcon: Icon(Icons.password),
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(13) 
+              ),
+              filled: true,
+              fillColor: Colors.grey[100]
             ),
             ),
-            ElevatedButton(onPressed: null, child: Text("Sign Up")),
+            SizedBox(height: 15,),
+            ElevatedButton(onPressed: null, child: Text("Sign Up"), style: ElevatedButton.styleFrom(
+                padding: EdgeInsets.symmetric(horizontal: 50, vertical: 8),
+                shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+                textStyle: TextStyle(fontSize: 20)
+                )),
             Text("Already have an account?"),
             TextButton(onPressed: () => {
               Navigator.pop(context)
