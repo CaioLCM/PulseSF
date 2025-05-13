38da206408f3140dd15a79c4efc336204c4d1d6b
Caio Lene Campos Magalhães <caio.lene.magalhaes@gmail.com>
2025-05-12 23:32:56 -0300

    Add the opions in main menu

--- lib/pages/mainPage.dart
+++ lib/pages/mainPage.dart
@@ -4,17 +4,79 @@ class Mainpage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("PulseSF"),),
       body: Column(
         children: [
-
-          Row(
-            children: [
-              Icon(Icons.place_outlined),
-              SizedBox(width: 10),
-              Text("Discover Events"),
-            ],
+          SizedBox(height: 40,),
+          Text("PulseSF", style: TextStyle(
+                color: Colors.purple[400],
+                fontSize: 40,
+                fontWeight: FontWeight.bold
+                )
+        ),
+          Container(
+             padding: EdgeInsets.all(16),
+            margin: EdgeInsets.only(bottom: 16),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(12),
+              color: Colors.grey[200]
+            ),
+            child: Row(
+              children: [
+                SizedBox(width: 10,),
+                Icon(Icons.place_outlined),
+                SizedBox(width: 10),
+                Text("Discover Events"),
+              ],
+            ),
           ),
+          Container(
+             padding: EdgeInsets.all(16),
+            margin: EdgeInsets.only(bottom: 16),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(12),
+              color: Colors.grey[200]
+            ),
+            child: Row(
+              children: [
+                SizedBox(width: 10,),
+                Icon(Icons.code),
+                SizedBox(width: 10),
+                Text("Match a coding buddy"),
+              ],
+            ),
+          ),
+          Container(
+            padding: EdgeInsets.all(16),
+            margin: EdgeInsets.only(bottom: 16),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(12),
+              color: Colors.grey[200]
+            ),
+            child: Row(
+              children: [
+                SizedBox(width: 10,),
+                Icon(Icons.precision_manufacturing),
+                SizedBox(width: 10),
+                Text("Create/join projects"),
+              ],
+            ),
+          ),
+                    Container(
+                       padding: EdgeInsets.all(16),
+            margin: EdgeInsets.only(bottom: 16),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(12),
+              color: Colors.grey[200]
+            ),
+                      child: Row(
+                                  children: [
+                                    SizedBox(width: 10,),
+                                    Icon(Icons.person),
+                                    SizedBox(width: 10),
+                                    Text("Profile"),
+                                  ],
+                                ),
+                    ),
         ],
       ),
     );
