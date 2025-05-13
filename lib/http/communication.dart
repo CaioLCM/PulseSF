f94d48ccbfbc701eb4c5dbe08ac4a86698ddcd24
Caio Lene Campos Magalhães <caio.lene.magalhaes@gmail.com>
2025-05-13 00:47:28 -0300

    Added the params to body of post

--- lib/http/communication.dart
+++ lib/http/communication.dart
@@ -3,8 +3,6 @@ import 'package:http/http.dart' as http;
 var url = Uri.parse('http://localhost:3000');
 
 void sendRequest() async {
-  var response = await http.post(url);
-  print('Response status: ${response.statusCode}');
-  print('Response text: ${response.body}');
+  var response = await http.post(url, body: {"nickname": 'none', 'email': 'none', 'password': 'none'});
 }
 
