e4a0784c98ee33c471efb244731728f87bd96782
Caio Lene Campos Magalhães <caio.lene.magalhaes@gmail.com>
2025-05-15 00:33:57 -0300

    Insert the token on the app

--- lib/http/communication.dart
+++ lib/http/communication.dart
@@ -1,7 +1,10 @@
+import 'dart:convert';
+
 import 'package:flutter/widgets.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:pulsesf/pages/mainPage.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 void CreateAccount(String nickname, String email, String password, BuildContext context) async {
   final url = Uri.parse('http://10.0.2.2:3000/logon');
@@ -15,6 +18,12 @@ void verifyAccount(String email, String password, BuildContext context) async {
   final url = Uri.parse('http://10.0.2.2:3000/login');
   var response = await http.post(url, body: {"email": email, "password": password});
   if (response.statusCode == 200){
+             final data = jsonDecode(response.body);
+             final token = data['token'];
+             SharedPreferences prefs = await SharedPreferences.getInstance();
+             await prefs.setString('jwt_token', token);
              Navigator.push(context, MaterialPageRoute(builder: (builder) => Mainpage()));
+             print(prefs.getString("jwt_token"));
         }
 }
+
